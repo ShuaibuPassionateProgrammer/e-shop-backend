6bbd69650d24e40e89a29c4e200e4ddc248513a2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-24 17:23:25 +0100

    Improving the frontend

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from './features/users/userSlice';
+import { usersApi } from './features/users/userSlice';
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
